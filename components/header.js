fdbed102fa141123a339f1a8ca0687f75cb2ffce
Tony Thomas <tony.thomas@schibsted.com>
2022-11-13 12:55:28 +0100

    Update header

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <h2 className="mb-20 mt-8 text-2xl font-bold leading-tight tracking-tight md:text-4xl md:tracking-tighter">
       <Link href="/">
-        <a className="hover:underline">Blog</a>
+        <a className="hover:underline">Tony's Space.</a>
       </Link>
       .
     </h2>
