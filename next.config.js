e1ba539232aca1115c661d8d91735df1cc77d908
Tony Thomas <tony.thomas@schibsted.com>
2022-11-13 12:15:09 +0100

    Ditch experimental edge

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  experimental: {
-    runtime: 'experimental-edge',
-  },
-  reactStrictMode: true,
-  swcMinify: true,
+  // experimental: {
+  //   runtime: 'experimental-edge',
+  // },
+  // reactStrictMode: true,
+  // swcMinify: true,
   images: {
     domains: ['cdn.sanity.io', 'source.unsplash.com'],
   },
